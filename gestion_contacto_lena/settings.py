a1e6a4735847797f9b2e1913328a8ae7c9515c1b
Javier Huerta <javierhuerta@unach.cl>
2014-10-17 14:41:54 -0300

    añadidas rutas para archivos estaticos y media

--- gestion_contacto_lena/settings.py
+++ gestion_contacto_lena/settings.py
@@ -92,7 +92,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+
 MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,  'media')
+
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
