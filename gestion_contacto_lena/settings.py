ddd6db5ffb33831786affdf2be599832c419a45e
Javier Huerta <javierhuerta91@gmail.com>
2015-04-07 15:01:03 -0300

    Cambios para cambio de contraseña

--- gestion_contacto_lena/settings.py
+++ gestion_contacto_lena/settings.py
@@ -111,5 +111,5 @@ TEMPLATE_DIRS = (
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gestiondedistribucondelena@gmail.com'
+EMAIL_HOST_USER = 'gestiondedistribuciondelena@gmail.com'
 EMAIL_HOST_PASSWORD = '20052015'
