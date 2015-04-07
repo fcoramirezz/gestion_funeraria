11d69b4aa071602747c07d00e5b73176099ba785
Javier Huerta <javierhuerta91@gmail.com>
2015-04-07 15:09:28 -0300

    test error mail send

--- gestion_contacto_lena/settings.py
+++ gestion_contacto_lena/settings.py
@@ -110,6 +110,6 @@ TEMPLATE_DIRS = (
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gestiondedistribuciondelena@gmail.com'
 EMAIL_HOST_PASSWORD = '20052015'
+EMAIL_PORT = 587
