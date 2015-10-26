760ea9d0ed38dc5bb99bf4797a8d414df57c0d08
fcoramirezz <fcoramirezz@gmail.com>
2015-10-26 18:50:13 -0300

    ups

--- gestion_lena/views.py
+++ gestion_lena/views.py
@@ -62,24 +62,7 @@ def pedido_list_segundo(request):
 
 
 
-def email(request):
-    if request.method == 'GET':
-        form = ContactForm()
-    else:
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            subject = form.cleaned_data['subject']
-            from_email = form.cleaned_data['from_email']
-            message = form.cleaned_data['message']
-            try:
-                send_mail(subject, message, from_email, ['fcoramirezz@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('thanks')
-    return render(request, "pagina.html", {'form': form})
 
-def thanks(request):
-    return HttpResponse('Thank you for your message.')
    
 
 ##########CONTACTO###################################
